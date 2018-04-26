09b1dd63846c4840bf8e92dc3fc903f9e09ea75f
Tobias Tschinkowitz <he4d@posteo.de>
2018-04-26 16:36:10 +0200

    refactoring

--- memfree.c
+++ memfree.c
@@ -7,8 +7,8 @@
 #include <sys/sysctl.h>
 
 #define pagetok(size) ((size) << pageshift)
-#define MAX_STRING_LENGTH	16 
-#define LOG1024 		10
+#define MAX_STRING_LENGTH	16
+#define LOG1024			10
 
 static int pageshift;
 
@@ -27,7 +27,7 @@ struct meminfo_s {
 char *
 format_k(int amt)
 {
-	char *ret = NULL; 
+	char *ret = NULL;
 	char tag = 'K';
         if (amt >= 10000) {
                 amt = (amt + 512) / 1024;
@@ -65,41 +65,65 @@ read_meminfo()
 	meminfo.swap_free = pagetok(uvmexp.swpages - uvmexp.swpginuse);
 }
 
+void
+usage()
+{
+	fprintf(stderr, "Usage output here...\n");
+	exit(EXIT_SUCCESS);
+}
+
+void
+print_memfree() {
+}
+
 int
 main(int argc, char *argv[])
 {
-	memset(&meminfo, 0, sizeof(struct meminfo_s));
 
 	if (pledge("stdio vminfo", NULL) == -1)
 		err(EXIT_FAILURE, "pledge");
 
+	int ch;
+	while ((ch = getopt(argc, argv, "vh")) != -1) {
+		switch (ch) {
+			case 'v':
+				fprintf(stderr, "memfree-"VERSION"\n");
+				return 0;
+			case 'h':
+				usage();
+			default:
+				break;
+		}
+	}
+
+	memset(&meminfo, 0, sizeof(struct meminfo_s));
 	read_meminfo();
 
 	if (pledge("stdio", NULL) == -1)
 		err(EXIT_FAILURE, "pledge");
 
 
-	printf("\ttotal\t\tused\t\tfree\t\tshared\t\tbuffers\t\tcached\n");	
-        printf("%-7s\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n", "Mem:",
-            format_k(meminfo.total),
-            format_k(meminfo.used),
-            format_k(meminfo.free),
-            format_k(meminfo.shared),
-            format_k(meminfo.buffers),
-            format_k(meminfo.cached)
+	printf("\ttotal\t\tused\t\tfree\t\tshared\t\tbuffers\t\tcached\n");
+	printf("%-7s\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n", "Mem:",
+		format_k(meminfo.total),
+		format_k(meminfo.used),
+		format_k(meminfo.free),
+		format_k(meminfo.shared),
+		format_k(meminfo.buffers),
+		format_k(meminfo.cached)
 	);
 
+	/* This could overflow.... Fix it */
 	int bufcache = meminfo.buffers + meminfo.cached;
-        printf(
-                "-/+ buffers/cache:\t%s\t\t%s\n", 
-                format_k(meminfo.used + bufcache),
-                format_k(meminfo.free - bufcache) 
+
+	printf("-/+ buffers/cache:\t%s\t\t%s\n",
+		format_k(meminfo.used + bufcache),
+		format_k(meminfo.free - bufcache)
 	);
-	printf(
-            "%-7s\t%s\t\t%s\t\t%s\n", "Swap:",
-            format_k(meminfo.swap_total),
-            format_k(meminfo.swap_used),
-            format_k(meminfo.swap_free)
+	printf("%-7s\t%s\t\t%s\t\t%s\n", "Swap:",
+		format_k(meminfo.swap_total),
+		format_k(meminfo.swap_used),
+		format_k(meminfo.swap_free)
 	);
 	return 0;
 }
