d8b56349978718499667ceb68cb310d0939ae5a9
Tobias Tschinkowitz <he4d@posteo.de>
2018-04-26 23:21:54 +0200

    added buffer infos

--- memfree.c
+++ memfree.c
@@ -1,31 +1,32 @@
-#include <unistd.h>
+/* See LICENSE for license details */
+#include <sys/types.h>
+#include <sys/mount.h>
+#include <sys/sysctl.h>
+
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <err.h>
-#include <sys/types.h>
-#include <sys/sysctl.h>
+#include <unistd.h>
 
 #define pagetok(size) ((size) << pageshift)
 #define LOG1024			10
 #define NUM_STRINGS 	 	11
 
-static int pageshift;
-
 struct meminfo_s {
 	int total;
 	int used;
 	int free;
-	int shared;
-	int buffers;
-	int cached;
+	int64_t buffers;
+	int64_t cached;
 	int swap_total;
 	int swap_used;
 	int swap_free;
 } meminfo;
 
-char *
-format_k(int amt)
+/* This was taken from the top(1) source code (slightly modified) */
+static char *
+format_k(int64_t amt)
 {
         static char retarray[NUM_STRINGS][16];
         static int  idx = 0;
@@ -42,33 +43,45 @@ format_k(int amt)
                         tag = 'G';
                 }
         }
-        snprintf(ret, sizeof(retarray[0]), "%d%c", amt, tag);
+        snprintf(ret, sizeof(retarray[0]), "%lld%c", amt, tag);
         return (ret);
 }
 
-void
+static void
 read_meminfo()
 {
 	static int uvmexp_mib[] = {CTL_VM, VM_UVMEXP};
+	static int bcstats_mib[] = {CTL_VFS, VFS_GENERIC, VFS_BCACHESTAT};
+	static int pageshift;
 	struct uvmexp uvmexp;
+	struct bcachestats bcstats;
 	size_t size;
 
 	size = sizeof(uvmexp);
 	if (sysctl(uvmexp_mib, 2, &uvmexp, &size, NULL, 0) < 0)
 		err(1, "sysctl");
 
+	size = sizeof(bcstats);
+	if (sysctl(bcstats_mib, 3, &bcstats, &size, NULL, 0) < 0)
+		err(1, "sysctl");
+
 	pageshift = uvmexp.pageshift;
 	pageshift -= LOG1024;
 
 	meminfo.total = pagetok(uvmexp.npages);
-	meminfo.used = pagetok(uvmexp.active);
+	meminfo.used = pagetok(uvmexp.npages - uvmexp.free);
 	meminfo.free = pagetok(uvmexp.free);
+
+	/* not sure yet about the following two buffers... */
+	meminfo.buffers = pagetok(bcstats.numbufs);
+	meminfo.cached = pagetok(bcstats.numbufpages);
+
 	meminfo.swap_total = pagetok(uvmexp.swpages);
-	meminfo.swap_used = pagetok(uvmexp.swpginuse);
-	meminfo.swap_free = pagetok(uvmexp.swpages - uvmexp.swpginuse);
+	meminfo.swap_used = pagetok(uvmexp.swpgonly);
+	meminfo.swap_free = pagetok(uvmexp.swpages - uvmexp.swpgonly);
 }
 
-void
+static void
 print_usage()
 {
 	fprintf(stderr, "Usage output here...\n");
@@ -102,21 +115,19 @@ main(int argc, char *argv[])
 
 
 	printf("\ttotal\t\tused\t\tfree\t\tshared\t\tbuffers\t\tcached\n");
-	printf("%-7s\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\n", "Mem:",
+	printf("%-7s\t%s\t\t%s\t\t%s\t\t0K\t\t%s\t\t%s\n", "Mem:",
 		format_k(meminfo.total),
 		format_k(meminfo.used),
 		format_k(meminfo.free),
-		format_k(meminfo.shared),
 		format_k(meminfo.buffers),
 		format_k(meminfo.cached)
 	);
 
-	/* This could overflow.... Fix it */
-	int bufcache = meminfo.buffers + meminfo.cached;
+	int64_t bufcache = meminfo.buffers + meminfo.cached;
 
 	printf("-/+ buffers/cache:\t%s\t\t%s\n",
-		format_k(meminfo.used + bufcache),
-		format_k(meminfo.free - bufcache)
+		format_k(meminfo.used - bufcache),
+		format_k(meminfo.free + bufcache)
 	);
 	printf("%-7s\t%s\t\t%s\t\t%s\n", "Swap:",
 		format_k(meminfo.swap_total),
